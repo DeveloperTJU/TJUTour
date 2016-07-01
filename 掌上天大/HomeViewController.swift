927682d6c5b17f910bfaa92573d1a87ce61e108d
hui <hui@hui.local>
2016-07-01 13:24:53 +0800

    hui

--- 掌上天大/HomeViewController.swift
+++ 掌上天大/HomeViewController.swift
@@ -15,6 +15,38 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     var navigationBlurView:UIVisualEffectView!
     var backgroundBlurView:UIVisualEffectView!
     var indexChanged = false
+    var isDataLoaded = false
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func loadData() {
+        self.isDataLoaded = true
+        coverflow = iCarousel(frame: CGRectMake(10, 100, self.view.bounds.width - 20, (UIScreen.mainScreen().bounds.width - 20) * 3 / 8 + 5))
+        coverflow.dataSource = self
+        coverflow.delegate = self
+        coverflow.type = .CoverFlow
+        coverflow.scrollToItemAtIndex((Buildings.count - 1) / 2, animated: false)
+        let tableViewFrame = CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height)
+        self.mainTableView = UITableView(frame: tableViewFrame, style: .Grouped)
+        self.mainTableView.backgroundColor = .whiteColor()
+        self.mainTableView.delegate = self
+        self.mainTableView.dataSource = self
+        self.mainTableView.backgroundColor = .clearColor()
+        self.mainTableView.separatorStyle = .None
+        self.view.addSubview(self.mainTableView)
+        for i in 0 ..< Buildings.count - 1{
+            if Buildings[i].images.count == 0{
+                Buildings[i].images.append(UIImage(data: NSData(contentsOfURL: NSURL(string: "\(RequestClient.URL)/building_pictures/\(Buildings[i].id)/\(Buildings[i].id).jpg")!)!)!)
+            }
+        }
+        coverflow.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +57,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         backgroundBlurView = UIVisualEffectView(effect: blurEffect)
         backgroundBlurView.frame.size = self.view.bounds.size
         self.view.addSubview(backgroundBlurView)
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         let mapButton = UIBarButtonItem(image: UIImage(named: "地图"), style: .Plain, target: self, action: Selector("openMap"))
         let searchButton = UIBarButtonItem(image: UIImage(named: "搜索"), style: .Plain, target: self, action: Selector("search"))
         self.navigationItem.leftBarButtonItems = [mapButton]
@@ -32,20 +65,6 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.navigationController?.navigationBar.translucent = true
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: .Default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
-        coverflow = iCarousel(frame: CGRectMake(10, 100, self.view.bounds.width - 20, (UIScreen.mainScreen().bounds.width - 20) * 3 / 8 + 5))
-        coverflow.scrollToItemAtIndex((Buildings.count - 1) / 2, animated: false)
-        coverflow.dataSource = self
-        coverflow.delegate = self
-        coverflow.type = .CoverFlow
-        let tableViewFrame = CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height)
-        self.mainTableView = UITableView(frame: tableViewFrame, style: .Grouped)
-        self.mainTableView.backgroundColor = .whiteColor()
-        self.mainTableView.delegate = self
-        self.mainTableView.dataSource = self
-        self.view.addSubview(self.mainTableView)
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
-        self.mainTableView.backgroundColor = .clearColor()
-        self.mainTableView.separatorStyle = .None
     }
     
     func openMap(){
@@ -68,11 +87,11 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         else {
             imageView = UIImageView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width * 2 / 3, UIScreen.mainScreen().bounds.width * 3 / 8))
         }
-        if Buildings[self.coverflow.currentItemIndex].images.count == 0{
+        if Buildings[index].images.count == 0{
             imageView.image = UIImage(named: "0")
         }
         else{
-            imageView.image = Buildings[self.coverflow.currentItemIndex].images[0]
+            imageView.image = Buildings[index].images[0]
         }
         let nameLabel = UILabel(frame: CGRectMake(0, UIScreen.mainScreen().bounds.width * 3 / 8, UIScreen.mainScreen().bounds.width * 2 / 3, 20))
         nameLabel.text = Buildings[index].name
@@ -82,16 +101,10 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         return imageView
     }
     
-//    func carouselCurrentItemIndexDidChange(carousel: iCarousel) {
-//        if self.mainTableView != nil{
-//            self.mainTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: .None)
-//        }
-//    }
-    
     func carouselDidEndScrollingAnimation(carousel: iCarousel) {
         if self.mainTableView != nil && indexChanged{
             indexChanged = false
-            self.mainTableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: .None)
+            self.mainTableView.reloadSections(NSIndexSet(index: 1), withRowAnimation: .Automatic)
         }
     }
     
@@ -103,26 +116,32 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section != 1{
+            return 0
+        }
         return Buildings.count <= 0 ? 0 : Buildings[self.coverflow.currentItemIndex].images.count
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return (UIScreen.mainScreen().bounds.width - 20) * 3 / 8 + 110
+        return section == 0 ? (UIScreen.mainScreen().bounds.width - 20) * 3 / 8 + 110 : 0
     }
     
     func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 5
+        return section == 1 ? 5 : 0
     }
     
     func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return coverflow
+        return section == 0 ? coverflow : UIView()
     }
     
     func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) ->UITableViewCell{
+        if indexPath.section != 1{
+            return UITableViewCell()
+        }
         let cell = BaseCell()
         cell.backgroundColor = UIColor.clearColor()
         cell.cellImage.image = Buildings[self.coverflow.currentItemIndex].images[indexPath.row]
@@ -132,12 +151,11 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return (UIScreen.mainScreen().bounds.width - 20) * 9 / 16 + 10
+        return indexPath.section == 1 ? (UIScreen.mainScreen().bounds.width - 20) * 9 / 16 + 10 : 0
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let detailVC = DetailViewController()
-        
         self.navigationController?.pushViewController(detailVC, animated: true)
     }
     
@@ -153,8 +171,8 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         navigationBlurView.frame.size = CGSize(width: view.frame.width, height: 64)
         self.navigationController?.view.addSubview(self.navigationBlurView)
         self.navigationController!.view.bringSubviewToFront((self.navigationController?.navigationBar)!)
-        self.navigationBlurView.alpha = self.mainTableView.contentOffset.y / 120
-        self.backgroundBlurView.alpha = self.mainTableView.contentOffset.y / 120
+        self.navigationBlurView.alpha = isDataLoaded ? self.mainTableView.contentOffset.y / 120 : 0
+        self.backgroundBlurView.alpha = isDataLoaded ? self.mainTableView.contentOffset.y / 120 : 0
     }
     
     override func viewWillDisappear(animated: Bool) {
