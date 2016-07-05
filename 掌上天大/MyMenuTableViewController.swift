4efcb22479a4fb350d77ab0078a5af5cb4b449bb
tjufe-jeson <wangtututu@126.com>
2016-07-06 00:22:50 +0800

    调整细节

--- 掌上天大/MyMenuTableViewController.swift
+++ 掌上天大/MyMenuTableViewController.swift
@@ -73,7 +73,7 @@ class MyMenuTableViewController: UIViewController, UITableViewDelegate,UITableVi
         if (cell == nil) {
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "CELL")
             cell!.backgroundColor = UIColor.clearColor()
-            cell!.textLabel?.textColor = UIColor.darkGrayColor()
+            cell!.textLabel?.textColor = UIColor.whiteColor()
             let selectedBackgroundView = UIView(frame: CGRectMake(0, 0, cell!.frame.size.width, cell!.frame.size.height))
             selectedBackgroundView.backgroundColor = UIColor.grayColor().colorWithAlphaComponent(0.2)
             cell!.selectedBackgroundView = selectedBackgroundView
