cd84310d13c95fc1b444b96461925c3cc3bbb34f
yangyaohua <1007531454@qq.com>
2016-07-06 11:15:49 +0800

    增加ImageHelper修改图片颜色，搜索界面改进

--- 掌上天大/SearchViewController.swift
+++ 掌上天大/SearchViewController.swift
@@ -18,8 +18,7 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.navigationController?.navigationBarHidden = true
-        self.view.backgroundColor = UIColor.whiteColor()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
         self.setSearchBar()
         self.setMainTableView()
     }
@@ -34,12 +33,10 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
     
     //设置搜索条
     func setSearchBar(){
-        //let leftBtn:UIBarButtonItem = UIBarButtonItem(title: "返回", style: .Plain, target: self, action: "OnBackBtnClicked")
-        //self.navigationItem.leftBarButtonItem = leftBtn
         let labelFrame:CGRect = CGRectMake(10, 18, 50, 44)
         let backBtn:UIButton = UIButton(frame: labelFrame)
         backBtn.setTitle("返回", forState: UIControlState.Normal)
-        backBtn.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        backBtn.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
         backBtn.addTarget(self, action: "OnBackBtnClicked", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(backBtn)
         let frame:CGRect = CGRectMake(70, 14, self.view.bounds.width-80, 48)
@@ -47,8 +44,12 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
         self.searchBar.searchBarStyle = .Minimal
         self.searchBar.delegate = self
         self.searchBar.userInteractionEnabled = true
+        self.searchBar.tintColor = UIColor.whiteColor()
+        self.searchBar.barTintColor = UIColor.whiteColor()
+        let searchFiled = searchBar.valueForKey("searchField") as! UITextField
+        searchFiled.layer.cornerRadius = 3
+        searchFiled.layer.backgroundColor = UIColor.whiteColor().CGColor
         self.view.addSubview(self.searchBar)
-        //self.navigationItem.titleView = self.searchBar
     }
     
     
@@ -86,7 +87,7 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
         self.mainTableView.dataSource = self
         self.mainTableView.delegate = self
         self.mainTableView.backgroundColor = UIColor(red: 244/255, green: 247/255, blue: 249/255, alpha: 1)
-        // self.mainTableView.backgroundColor  = UIColor.orangeColor()
+        self.mainTableView.backgroundColor  = UIColor.clearColor()
         self.view.addSubview(mainTableView)
     }
     
@@ -109,13 +110,13 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
         if searchState == 1{
             if indexPath.row == 0{
                 let textWidth:CGFloat = 100.0//文本标签宽度
-                let textX:CGFloat = self.view.frame.width/3//文本标签x值
                 let imageWidth:CGFloat = 20.0//iamgeView的宽度
+                let textX:CGFloat = (self.view.frame.width-textWidth-imageWidth)/2//文本标签x值
                 //添加文本
                 let textframe:CGRect = CGRectMake(textX, 0, textWidth, cell.frame.height)
-                var textLabel:UILabel = UILabel(frame: textframe)
+                let textLabel:UILabel = UILabel(frame: textframe)
                 textLabel.text = "历史搜索"
-                textLabel.textColor = UIColor.grayColor()
+                textLabel.textColor = UIColor.whiteColor()
                 textLabel.textAlignment = .Center
                 cell.addSubview(textLabel)
                 
@@ -123,7 +124,9 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
                 let imageFrame:CGRect = CGRectMake(textWidth+textX, (cell.frame.height-imageWidth)/2, imageWidth, imageWidth)
                 let imageView:UIImageView = UIImageView(frame: imageFrame)
                 let image = UIImage(named: "删除")
-                imageView.image = image
+                
+                let colorImage = image?.tint(UIColor.whiteColor(), blendMode: .DestinationIn)
+                imageView.image = colorImage
                 let gesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "onDeleteHistorySearch:")
                 imageView.userInteractionEnabled = true
                 imageView.addGestureRecognizer(gesture)
@@ -135,29 +138,29 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
                 
                 let leftFrame:CGRect = CGRectMake(10, cell.frame.height/2, textX+distance-10, 1)
                 let leftView:UIView = UIView(frame: leftFrame)
-                leftView.backgroundColor = UIColor.grayColor()
+                leftView.backgroundColor = UIColor.whiteColor()
                 cell.addSubview(leftView)
                 
                 let centerFrame:CGRect = CGRectMake(textX+textWidth-distance, cell.frame.height/2, distance, 1)
                 let centerView:UIView = UIView(frame: centerFrame)
-                centerView.backgroundColor = UIColor.grayColor()
+                centerView.backgroundColor = UIColor.whiteColor()
                 cell.addSubview(centerView)
                 
                 let rightFrame:CGRect = CGRectMake(textX+textWidth+imageWidth, cell.frame.height/2, self.view.frame.width-(textX+textWidth+imageWidth)-10, 1)
                 let rightView:UIView = UIView(frame: rightFrame)
-                rightView.backgroundColor = UIColor.grayColor()
+                rightView.backgroundColor = UIColor.whiteColor()
                 cell.addSubview(rightView)
             }else{
                 cell.textLabel?.drawTextInRect(CGRectMake(100, 0, self.mainTableView.bounds.width-50, 0))
                 cell.textLabel?.text = "   " + historyArr[self.historyArr.count - indexPath.row]
-                cell.textLabel?.textColor = UIColor.grayColor()
+                cell.textLabel?.textColor = UIColor.whiteColor()
             }
             return cell
         }
         else if searchState == 0{
             //搜索状态，显示当前搜索
             cell.textLabel!.text = self.currentArr[indexPath.row].name
-            cell.textLabel!.textColor = UIColor.grayColor()
+            cell.textLabel!.textColor = UIColor.whiteColor()
             cell.accessoryType = .DisclosureIndicator
             return cell
         }else{
@@ -216,7 +219,7 @@ class SearchViewController: UIViewController ,UISearchBarDelegate,UITableViewDel
         return allBuildings
     }
     
-    
+    //搜索数据
     func filterContentForSearchText(searchText: String) {
         currentArr = self.currentArr.filter({ (buildingModel:BuildingData) -> Bool in
             let nameMatch = buildingModel.name.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
